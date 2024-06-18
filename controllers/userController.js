eb9f95d0fbb8cf2b0b6b0e9584237d6b4699b52a
easymoneyspike <euryanspike.pro@gmail.com>
2024-06-18 11:12:33 -0300

    app base com definição de rotas

---
+++ controllers/userController.js
@@ -0,0 +1,4 @@
+export const addUser = async (req, res) => {
+    return res.status(201).json({ message: 'usuário cadastrado.' })
+  
+}
---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+
+import userRoutes from './routes/userRoutes.js';
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+//compatibilidade
+app.use(express.json());
+
+
+//rotas definidas
+app.use('/cadastro', userRoutes);
+
+//rota home
+app.get('/', (req, res) => {
+    return res.status(200).json({ message: 'bem-vindo a home.' })
+})
+
+app.listen(port, () => {
+    console.log('server status: on-line')
+})
